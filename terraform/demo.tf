68809775ab7274e37fb403c63cf6a6aad3808612
Dan Iverson <dan@Kallies-MacBook.local>
2018-05-07 09:37:35 -0500

    add demo

--- terraform/demo.tf
+++ terraform/demo.tf
@@ -1,16 +1,16 @@
 # AWS Defaults
-// provider "aws" {
-//     region = "us-east-1"
-// }
+provider "aws" {
+    region = "us-east-1"
+}
 
 # Amazon Linux Instance
-// resource "aws_instance" "demo" {
-//     ami           = "ami-467ca739" 
-//     instance_type = "t2.large"
-//     count         = "1" 
-// }
+resource "aws_instance" "demo" {
+    ami           = "ami-467ca739" 
+    instance_type = "t2.large"
+    count         = "1" 
+}
 
 # Display Public DNS
-// output "address" {
-//   value = "${aws_instance.demo.public_dns}"
-// }
+output "address" {
+  value = "${aws_instance.demo.public_dns}"
+}
